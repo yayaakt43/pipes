0486c959304c09f5c858569cdf0696588bb66ba4
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2026-01-14 19:51:27 +0100

    Update ecrivain.c
    len

--- ecrivain.c
+++ ecrivain.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]){
   /* Création fichier FIFO */
   mkfifo("fictub",mode);
   int tub = open("fictub",O_WRONLY);
-  write(tub,"Bonjour tout le monde !",73);
+  write(tub,"Bonjour tout le monde ! L'été sera chaud cette année en 2025 ici à Paris",73);
   close(tub);
 
 return 0;
